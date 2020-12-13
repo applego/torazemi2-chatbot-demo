4e3373b0a9c8670300baac84185b028e3845da78
applego <applegorillappa@gmail.com>
2020-12-14 00:08:12 +0900

    ・マテリアルUIのボタン導入 ・AnswersListコンポーネントからAnswerにpropsを渡す（content)

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+// https://material-ui.com/components/buttons/#contained-buttons (参照)
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
-const Answer = () => {
+const useStyles = makeStyles((theme) => ({
+  root: {
+  },
+}));
+
+
+const Answer = (props) => {
+  // const classes = useStyles();
   return (
-
+    <div>
+      <Button variant="contained" color="primary">
+        {props.content}
+      </Button>
+    </div>
   )
 }
 
