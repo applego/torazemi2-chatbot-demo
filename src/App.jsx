e8af49e78e0062ac8b783309837ce50398cbc67d
applego <applegorillappa@gmail.com>
2020-12-15 00:28:29 +0900

    SelectAnswerメソッドをbind renderの度にコールバック関数が作成されるのを防ぐ（パフォーマンス）

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ class App extends React.Component {
       dataset: defaultDataset,
       open: false
     }
+    this.selectAnswer = this.selectAnswer.bind(this);
   }
 
   displayNextQuestion = (nextQuestionId) => {
@@ -63,7 +64,7 @@ class App extends React.Component {
       <section className="c-section">
         <div className="c-box">
           <Chats chats={this.state.chats} />
-          <AnswersList answers={this.state.answers} />
+          <AnswersList answers={this.state.answers} select={this.selectAnswer} />
         </div>
       </section>
     );
--- src/components/AnswersList.jsx
+++ src/components/AnswersList.jsx
@@ -5,7 +5,7 @@ const AnswersList = (props) => {
   return (
     <div className="c-grid__answer">
       {props.answers.map((answer, index) => {
-        return <Answer content={answer.content} key={ index.toString() }/>
+        return <Answer content={answer.content} nextId={answer.nextId} key={index.toString()} select={ props.select }/>
       })}
     </div>
   )
