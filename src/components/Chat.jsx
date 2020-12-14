d315cd4db0f5166efd9afa7f00626e7c873f6a50
applego <applegorillappa@gmail.com>
2020-12-14 17:52:13 +0900

    <ListItem>（マテリアルUI）をChatsからChatに移動 ・未使用のコンポーネント？（マテリアル）を削除 ・テキストにはp-chat__bubbleクラスを指定

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
 
 const Chat = () => {
-  return(
-
+  return (
+    <ListItem>
+      <ListItemAvatar>
+        <Avatar alt="icon" src="/static/images/avatar/1.jpg" />
+      </ListItemAvatar>
+      <div className="p-chat__bubble">ダミーテキスト</div>
+    </ListItem>
   )
 }
 
