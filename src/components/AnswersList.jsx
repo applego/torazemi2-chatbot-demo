6809b0cf53a1cf8a375a962621c42f5db67d4c26
applego <applegorillappa@gmail.com>
2020-12-14 18:07:31 +0900

    AnswersListからAnswerにprops.answersを渡すときの変数名を変更

--- src/components/AnswersList.jsx
+++ src/components/AnswersList.jsx
@@ -4,8 +4,8 @@ import { Answer } from './index';
 const AnswersList = (props) => {
   return (
     <div className="c-grid__answer">
-      {props.answers.map((value, index) => {
-        return <Answer content={value.content} key={ index.toString() }/>
+      {props.answers.map((answer, index) => {
+        return <Answer content={answer.content} key={ index.toString() }/>
       })}
     </div>
   )
