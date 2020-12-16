85703789325ef37a3b564f78fed139292e35d58e
applego <applegorillappa@gmail.com>
2020-12-17 01:10:49 +0900

    Material-UI Answerのスタイルが左寄せになってたのは、Answerコンポーネントの戻り値に<div>が<Button>の親要素としてあったからだった。
    const useStyles = makeStyles(() =>
      createStyles({
        "button": {
          borderColor:'#FFB549',
          color:'#FFB549',
          fontWeight: 600,
          marginBottom: '8px',
          "&:hover":{
            backgroundColor:'#FFB549',
            color:'#fff'
          }
        }
      })
    );
    
    const Answer = (props) => {
      const classes = useStyles();
      return (
        <Button
          className={classes.button}
          variant="outlined" onClick={() => props.select(props.content, props.nextId)}>
          {props.content}
          </Button>

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
 // https://material-ui.com/components/buttons/#contained-buttons (参照)
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-  },
-}));
+const useStyles = makeStyles(() =>
+  createStyles({
+    "button": {
+      borderColor:'#FFB549',
+      color:'#FFB549',
+      fontWeight: 600,
+      marginBottom: '8px',
+      "&:hover":{
+        backgroundColor:'#FFB549',
+        color:'#fff'
+      }
+    }
+  })
+);
 
 
 const Answer = (props) => {
-  // const classes = useStyles();
+  const classes = useStyles();
   return (
-    <div>
-      <Button variant="contained" color="primary" onClick={()=>props.select(props.content, props.nextId)}>
-        {props.content}
+    <Button
+      className={classes.button}
+      variant="outlined" onClick={() => props.select(props.content, props.nextId)}>
+      {props.content}
       </Button>
-    </div>
   )
 }
 
