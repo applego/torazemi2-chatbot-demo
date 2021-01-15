bbe5e4c2a8aecaf116871891c9037d2c847dc618
applego <applegorillappa@gmail.com>
2021-01-15 22:49:57 +0900

    Formsにindex.jsを追加 (cherry pick)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect,useCallback } from 'react';
 import './assets/styles/style.css';
-import { AnswersList, Chats } from './components/index';
-import FormDialog from './components/Forms/FormDialog';
+import { AnswersList, Chats, Loading } from './components/index';
+import { FormDialog } from './components/Forms/index';
+import { db } from './firebase/index';
 
 const App = () => {
   const [answers, setAnswers] = useState([]);
@@ -104,8 +105,14 @@ const App = () => {
   return (
     <section className="c-section">
       <div className="c-box">
-        <Chats chats={chats} />
-        <AnswersList answers={answers} select={selectAnswer} />
+        {(Object.keys(dataset).length === 0) ? (
+          <Loading />
+        ) : (
+            <>
+              <Chats chats={chats} />
+              <AnswersList answers={answers} select={selectAnswer} />
+            </>
+        )}
         <FormDialog open={open} handleClose={handleClose} />
       </div>
     </section>
